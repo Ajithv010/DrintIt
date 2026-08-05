ee011d30a0caee1e6f21637c6248cd41390594b7
Ajithv010 <ajithvenkatesh1234@gmail.com>
2026-08-05 18:02:02 +0530

    Initial project setup

---
+++ src/test/java/com/ajith/DrinkIt/DrinkItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajith.DrinkIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
